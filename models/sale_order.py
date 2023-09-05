23caeceb59bf643c7dbf761a33c87b7dc76a02ae
rocketgithub <rodrigo@aquih.com>
2023-09-05 17:34:19 -0600

    No usar related y definir campo en el módulo.

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,12 +3,13 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    dias = fields.Integer(string="Días")
     precio_unitario_dias = fields.Float(string="Precio unitario por día")
 
     @api.onchange("product_id")
-    def _onchange_product_id_para_precio(self):
+    def _onchange_product_id(self):
         self.precio_unitario_dias = self.product_id.list_price
 
-    @api.onchange("x_studio_das", "precio_unitario_dias")
-    def _onchange_price_para_precio(self):
-        self.price_unit = self.x_studio_das * self.precio_unitario_dias
+    @api.onchange("dias", "precio_unitario_dias")
+    def _onchange_price(self):
+        self.price_unit = self.dias * self.precio_unitario_dias
