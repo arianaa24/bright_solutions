9d76335ee83816a8fd70bd50bdb81e7ef4ca4484
arianaa24 <arianacarrillo24@gmail.com>
2023-09-05 18:28:09 +0000

    regreso de campo precio_unitario_dias

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,8 +3,8 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    precio_unitario_dia = fields.Float(string="Precio unitario por día", related="product_template_id.list_price", readonly=False, store=True)
+    precio_unitario_dias = fields.Float(string="Precio unitario por día", related="product_template_id.list_price", readonly=False, store=True)
 
-    @api.onchange("x_studio_das", "precio_unitario_dia", "product_uom_qty")
+    @api.onchange("x_studio_das", "precio_unitario_dias", "product_uom_qty")
     def _onchange_price(self):
-        self.price_unit = self.x_studio_das * self.precio_unitario_dia
+        self.price_unit = self.x_studio_das * self.precio_unitario_dias
