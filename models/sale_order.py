7fab46e4c9a529b147c4dc26527887efe94e4d22
rocketgithub <rodrigo@aquih.com>
2023-09-05 16:20:12 -0600

    Código más sencillo

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,8 +3,12 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    precio_unitario_dias = fields.Float(string="Precio unitario por día", related="product_template_id.list_price", readonly=False, store=True)
+    precio_unitario_dias = fields.Float(string="Precio unitario por día")
 
-    @api.onchange("x_studio_das", "precio_unitario_dias", "product_uom_qty")
-    def _onchange_price(self):
+    @api.onchange("product_id")
+    def _onchange_product_id_para_precio(self):
+        self.precio_unitario_dias = self.product_id.list_price
+
+    @api.onchange("x_studio_das", "precio_unitario_dias")
+    def _onchange_price_para_precio(self):
         self.price_unit = self.x_studio_das * self.precio_unitario_dias
