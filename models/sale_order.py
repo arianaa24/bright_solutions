bebdb0cc9fdc7a79af1147e764f50f58929c1380
arianaa24 <arianacarrillo24@gmail.com>
2023-10-10 06:28:38 +0000

    cambios en columnas en SO

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,7 +4,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     dias = fields.Integer(string="Días")
-    precio_unitario_dias = fields.Float(string="Precio unitario por día")
+    precio_unitario_dias = fields.Float(string="Precio unitario por día", digits=(12,3))
     costo_total = fields.Monetary(string="Costo Total")
     vc = fields.Monetary(string="VC")
     margen = fields.Float(string="Margen %")
@@ -13,11 +13,11 @@ class SaleOrderLine(models.Model):
     def _onchange_product_id(self):
         self.precio_unitario_dias = self.product_id.list_price
 
-    @api.onchange("dias", "precio_unitario_dias")
+    @api.onchange("dias", "precio_unitario_dias","product_uom_qty")
     def _onchange_price(self):
         self.price_unit = self.dias * self.precio_unitario_dias
 
-    @api.onchange("x_studio_costo")
+    @api.onchange("x_studio_costo","precio_unitario_dias", "product_uom_qty", "dias")
     def _onchange_product_id(self):
         self.costo_total = self.product_uom_qty * self.dias * self.x_studio_costo
         self.vc = self.price_subtotal - self.costo_total
